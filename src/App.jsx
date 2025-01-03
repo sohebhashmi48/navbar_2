5a909f4aae68ec2b3aa94d6549dd5d973fb39411
sohebhashmi48 <sohebhashmi48@gmail.com>
2025-01-03 20:17:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from './navbar'
+import './navbar.css'
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/navbar.jsx
@@ -0,0 +1,26 @@
+function Nav(){
+        return(
+            <>
+                <div className="container">
+                    <nav>
+                        <div className="logo">
+                            V<span>3</span>NOM
+                        </div>
+                        <div className="nav-left">
+                            <ul>
+                                    <li className="glow"><a href="#"></a>Home</li>
+                                    <li className="glow"><a href="#"></a>Blog</li>
+                                    <li className="glow"><a href="#"></a>Contact</li>
+                                    <li className="glow"><a href="#"></a>Service</li>
+                                    <li className="glow"><a href="#"></a>About</li>
+                                </ul>
+                        </div>
+                        <div className="btn">
+                            <button>Suscribe</button>
+                        </div>
+                    </nav>
+                </div>
+            </>
+        )
+}
+export default Nav
